0cf91f096eae444f160942b0b51cf5df39037d7b
Juan Tenezaca <tenj34@gmail.com>
2018-06-03 18:59:55 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,9 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 def display_board
-	puts "   "
+3.times{
+  puts "   "
 	puts "   |   |   "
 	puts "-----------"
   puts "   "
+}
 end
